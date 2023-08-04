cecc6dc6afcb3a39f95d6c0479f26c184699fc89
Ayush123rajawat <97392316+Ayush123rajawat@users.noreply.github.com>
2023-08-05 00:08:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
+require('dotenv').config();
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 const app = express();
 const port = 7000 || process.env.PORT;
+
+app.use(expressLayouts);
+app.set('layout','./layouts/main');
+app.set('view engine','ejs');
+
+app.use('/',require('./server/routes/customer'));
 
 app.get('*',(req,res)=>{
    res.status(404).render('404');
